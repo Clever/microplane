9553d7bbe7558ccdcb13ac91dd06e89a5d9044ff
Nathan Leiby <nathan.leiby@clever.com>
2017-10-19 16:51:33 -0700

    cmd/status: dumb starting impl

--- cmd/status.go
+++ cmd/status.go
@@ -2,7 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path"
 
+	"github.com/Clever/microplane/initialize"
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +16,26 @@ var statusCmd = &cobra.Command{
 	Long: `status
                 long
                 description`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("status")
+		target := args[0]
+		// find files and folders to explain the status of each repo
+		initPath := path.Join(workDir, "/", target, "/init.json")
+
+		if _, err := os.Stat(initPath); os.IsNotExist(err) {
+			log.Fatalf("target not found: %s\n", err.Error())
+		}
+
+		var initOutput initialize.Output
+		if err := loadJSON(initPath, &initOutput); err != nil {
+			log.Fatalf("error loading init.json: %s\n", err.Error())
+		}
+
+		// TODO: pretty print status
+		fmt.Printf("%40s    %20s    %20s\n", "repo", "status", "details")
+		fmt.Printf("%40s    %20s    %20s\n", "====", "======", "=======")
+		for _, r := range initOutput.Repos {
+			fmt.Printf("%40s    %20s    %20s\n", r.Name, "initialized", "...")
+		}
 	},
 }
