f4d640ebc9f35f0b4929b6881af20b5cd22c10e5
Ade Attwood <ade@practically.io>
2021-08-13 11:39:15 +0100

    refactor: the Gitlab PR body to be consistent with Github
    Now both Gitlab and Github have the same logic for creating the PR title
    and body. It will use the first line of the commit as the title then
    concatenate the commit body and the content of the PRBody file that is
    passed in as a parameter

--- push/pushGitlab.go
+++ push/pushGitlab.go
@@ -48,19 +48,7 @@ func GitlabPush(ctx context.Context, input Input, repoLimiter *time.Ticker, push
 	head := input.BranchName
 	base := project.DefaultBranch
 
-	// Determine MR title and body
-	// Title is first line of commit message.
-	// Body is given by body-file if it exists or is the remainder of the commit message after title.
-	title := input.CommitMessage
-	body := ""
-	splitMsg := strings.SplitN(input.CommitMessage, "\n", 2)
-	if len(splitMsg) == 2 {
-		title = splitMsg[0]
-		if input.PRBody == "" {
-			body = splitMsg[1]
-		}
-	}
-
+	title, body := getTitleBody(input)
 	pr, err := findOrCreateGitlabMR(ctx, client, input.Repo.Owner, input.Repo.Name, &gitlab.CreateMergeRequestOptions{
 		Title:        &title,
 		Description:  &body,
